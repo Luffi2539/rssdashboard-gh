fcf47e4035939146844a77379289c0c421c270ac
Viktor Kovalev <vkkovalev91@gmail.com>
2019-02-18 04:19:02 +0300

    feat: add last version of build

---
+++ build/precache-manifest.8e013f33b858d344fbd89d5c3229a0d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "e36fccc9a1f949d9998c",
+    "url": "./static/js/main.e36fccc9.chunk.js"
+  },
+  {
+    "revision": "4eee753e8c56e1785343",
+    "url": "./static/js/2.4eee753e.chunk.js"
+  },
+  {
+    "revision": "e36fccc9a1f949d9998c",
+    "url": "./static/css/main.e7d5e461.chunk.css"
+  },
+  {
+    "revision": "9053f212736af2272677e52f723bd3b4",
+    "url": "./index.html"
+  }
+];
